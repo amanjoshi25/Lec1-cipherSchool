6b735e498b8013904fe931d02b12469463c43530
Aman Joshi <amanjoshi2527@gmail.com>
2023-06-24 15:46:01 +0530

    First commit by code

---
+++ Lecture1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int n1,int n2){
+    return n1+n2;
+}
+int main(){
+    cout<<"Hello World Aman Joshi is in git Hub now\n";
+ 
+ int n1,n2;
+ cout<<"value of N1=";
+ cin>>n1;
+ cout<<"\nvalue of N2=";
+ cin>>n2;
+ cout<<sum(n1,n2);
+    return 0;
+}
